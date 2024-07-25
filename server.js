346a356ff2c5e6c15b5d945e65d67b163fb2f1ea
Jordan Forbes <jordforb@gmail.com>
2024-07-25 10:15:01 -0400

    fixed connect-redis error

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 const mongoose = require("mongoose");
 const session = require("express-session");
-const RedisStore = require("connect-redis")(session);
+const RedisStore = require("connect-redis").default;
 const redis = require("redis");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
@@ -18,50 +18,51 @@ const io = socketIo(server);
 const redisCli = redis.createClient(); // creat Redis Client
 const SESSION_SECRET = "foobarbaz"; // placeholder key
 
-
 // configure session middleware
-app.use(session({
-  store: new RedisStore({ client: redisCli}),
-  secret: SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {secure: false }
-}))
+app.use(
+  session({
+    store: new RedisStore({ client: redisCli }),
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
 
 app.use(express.json()); //middleware to parse JSON bodies
 
 // define mongoose Schemas and models
-mongoose.connect('mongodb://localhost/chatapp',{
+mongoose.connect("mongodb://localhost/chatapp", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+  useUnifiedTopology: true,
+});
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, unique: true },
-  password: String;
+  password: String,
 });
 
 const MessageSchema = new mongoose.Schema({
   user: String,
   message: String,
-  timestamp: { type: Date, default: Date.now()}
-})
+  timestamp: { type: Date, default: Date.now() },
+});
 
 // create model instances
-const User = mongoose.model('User', UserSchema);
-const Message = mongoose.model('Message', MessageSchema);
+const User = mongoose.model("User", UserSchema);
+const Message = mongoose.model("Message", MessageSchema);
 
-// routes
-// registration routes
-app.post('/register', async (req, res)=>{
-  const {username, password} = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10); //hash password
-  const user = new User({ username, password: hashedPassword});
-  await user.save(); // save user to database
-  res.status(201).send('User registered successfully'); //send success response
-})
+// // routes
+// // registration routes
+// app.post('/register', async (req, res)=>{
+//   const {username, password} = req.body;
+//   const hashedPassword = await bcrypt.hash(password, 10); //hash password
+//   const user = new User({ username, password: hashedPassword});
+//   await user.save(); // save user to database
+//   res.status(201).send('User registered successfully'); //send success response
+// })
 
 // start server
-server.listen(3000,()=>{
-  console.log('server is listening on port 3000');
-})
+server.listen(3000, () => {
+  console.log("server is listening on port 3000");
+});
